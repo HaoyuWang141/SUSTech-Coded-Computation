d9ef4e1bee18ae59bced82ba097359adec0cb9db
YiyuLiu-Rin <rinland@qq.com>
2023-12-20 11:57:26 +0800

    [update] data/download.py,split_data.py (Add model VGG16 and dataset CIFAR10)

--- src/data/download.py
+++ src/data/download.py
@@ -3,6 +3,8 @@
 import torchvision
 from torchvision import datasets, transforms
 
+# MINIST
+
 # Transform to convert images to PyTorch tensors
 transform = transforms.Compose([transforms.ToTensor()])
 
@@ -11,3 +13,18 @@ train_set = datasets.MNIST(root="./data", train=True, download=True, transform=t
 
 # Downloading the MNIST testing dataset
 test_set = datasets.MNIST(root="./data", train=False, download=True, transform=transform)
+
+
+# CIFAR10
+
+# Define the transformation
+transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+])
+
+# Downloading the CIFAR10 training dataset
+train_set = datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
+
+# Downloading the CIFAR10 testing dataset
+test_set = datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
--- src/data/split_data.py
+++ src/data/split_data.py
@@ -5,82 +5,69 @@ Split the original dataset into our form.
 from torchvision import datasets, transforms
 from util.split_data import split_data
 from base_model.LeNet5 import LeNet5
+from base_model.VGG16 import VGG16
 import torch
 
-# TODO: change your base model
-model = LeNet5(input_dim=(1, 28, 28), num_classes=10)
+# TODO: change your base model and choose a trained model instance
 
-# TODO: change your training file path of base model
-base_model_path = "base_model/LeNet5/MNIST/2023_11_28/model.pth"
+# LeNet5
+model = LeNet5(input_dim=(1, 28, 28), num_classes=10)
+base_model_path = "../base_model/LeNet5/MNIST/2023_11_28/model.pth"
 model.load_state_dict(torch.load(base_model_path))
 
-# TODO: change your transform
+# VGG16
+# model = VGG16(input_dim=(3, 32, 32), num_classes=10)
+# base_model_path = "../base_model/VGG16/CIFAR10/2023_11_28/model.pth"
+# model.load_state_dict(torch.load(base_model_path))
+
+
+# TODO: change your dataset and transform
+
 transform = transforms.Compose(
     [transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))]
 )
 
-# TODO: change your dataset path
-dataset = datasets.MNIST(root="./data", train=False, download=True, transform=transform)
-print(f"Test dataset: {len(dataset)}")
-print("image size: ", dataset[0][0].size())
+# LeNet5
+dataset_name = "MNIST"
+test_dataset = datasets.MNIST(root="./", train=False, download=True, transform=transform)
+train_dataset = datasets.MNIST(root="./", train=True, download=True, transform=transform)
 
-# TODO: change your split data params: split_num, train (true=train, false=test), and output_file
-split_data(
-    layers=model.get_conv_segment(),
-    dataset=dataset,
-    split_num=1,
-    train=False,
-    output_file=f"./data/MNIST/split/1/split_test_datasets.pt",
-)
+# VGG16
+# dataset_name = "CIFAR10"
+# test_dataset = datasets.CIFAR10(root="./", train=False, download=True, transform=transform)
+# train_dataset = datasets.CIFAR10(root="./", train=True, download=True, transform=transform)
 
-# print("-" * 50)
 
-# split_data(
-#     layers=model.get_conv_segment(),
-#     dataset=dataset,
-#     split_num=2,
-#     train=False,
-#     output_file=f"./data/MNIST/split/2/split_test_datasets.pt",
-# )
+# TODO: change your list of split numbers
+split_nums = [2]  # you may choose from [1, 2, 4, ..]
 
-# print("-" * 50)
 
-# split_data(
-#     layers=model.get_conv_segment(),
-#     dataset=dataset,
-#     split_num=4,
-#     train=False,
-#     output_file=f"./data/MNIST/split/4/split_test_datasets.pt",
-# )
+print(f"Test dataset: {len(test_dataset)}")
+print("image size: ", test_dataset[0][0].size())
+print(f"Train dataset: {len(train_dataset)}")
+print("image size: ", train_dataset[0][0].size())
 
-# print("-" * 50)
 
-# dataset = datasets.MNIST(root="./data", train=True, download=True, transform=transform)
-# print(f"Train dataset: {len(dataset)}")
-# print("image size: ", dataset[0][0].size())
+# Split test dataset
 
-# split_data(
-#     layers=model.get_conv_segment(),
-#     dataset=dataset,
-#     split_num=1,
-#     train=True,
-#     output_file=f"./data/MNIST/split/1/split_train_datasets.pt",
-# )
+for split_num in split_nums:
+    split_data(
+        layers=model.get_conv_segment(),
+        dataset=test_dataset,
+        split_num=split_num,
+        train=False,
+        output_file=f"./{dataset_name}/split/{split_num}/split_test_datasets.pt",
+    )
+    print("-" * 50)
 
-# split_data(
-#     layers=model.get_conv_segment(),
-#     dataset=dataset,
-#     split_num=2,
-#     train=True,
-#     output_file=f"./data/MNIST/split/2/split_train_datasets.pt",
-# )
+# Split train dataset
 
-# print("-" * 50)
-
-# split_data(
-#     layers=model.get_conv_segment(),
-#     dataset=dataset,
-#     split_num=4,
-#     train=True,
-#     output_file=f"./data/MNIST/split/4/split_train_datasets.pt",
-# )
+for split_num in split_nums:
+    split_data(
+        layers=model.get_conv_segment(),
+        dataset=train_dataset,
+        split_num=split_num,
+        train=True,
+        output_file=f"./{dataset_name}/split/{split_num}/split_train_datasets.pt",
+    )
+    print("-" * 50)
