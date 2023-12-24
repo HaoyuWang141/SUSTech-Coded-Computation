7bc0451331eeb735c2fc3f51253e55159c99a00a
YiyuLiu-Rin <rinland@qq.com>
2023-12-24 21:26:07 +0800

    [add] base_model/ResNet18; [update] test/train_base_model.ipynb

---
+++ src/base_model/ResNet18.py
@@ -0,0 +1,204 @@
+import math
+import torch
+import torch.nn as nn
+from base_model.BaseModel import BaseModel
+
+
+def conv3x3(in_planes, out_planes, stride=1):
+    "3x3 convolution with padding"
+    return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride, padding=1, bias=False)
+
+class BasicBlock(nn.Module):
+    expansion = 1
+
+    def __init__(self, inplanes, planes, stride=1, downsample=None):
+        super(BasicBlock, self).__init__()
+        self.conv1 = conv3x3(inplanes, planes, stride)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = conv3x3(planes, planes)
+        self.bn2 = nn.BatchNorm2d(planes)
+        self.downsample = downsample
+        self.stride = stride
+
+    def forward(self, x):
+        residual = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+
+        if self.downsample is not None:
+            residual = self.downsample(x)
+
+        out += residual
+        out = self.relu(out)
+
+        return out
+
+class Bottleneck(nn.Module):
+    expansion = 4
+
+    def __init__(self, inplanes, planes, stride=1, downsample=None):
+        super(Bottleneck, self).__init__()
+        self.conv1 = nn.Conv2d(inplanes, planes, kernel_size=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=stride,
+                               padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(planes)
+        self.conv3 = nn.Conv2d(planes, planes * 4, kernel_size=1, bias=False)
+        self.bn3 = nn.BatchNorm2d(planes * 4)
+        self.relu = nn.ReLU(inplace=True)
+        self.downsample = downsample
+        self.stride = stride
+
+    def forward(self, x):
+        residual = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+        out = self.relu(out)
+
+        out = self.conv3(out)
+        out = self.bn3(out)
+
+        if self.downsample is not None:
+            residual = self.downsample(x)
+
+        out += residual
+        out = self.relu(out)
+
+        return out
+
+
+class ResNet18(BaseModel):
+
+    def __init__(self, input_dim: tuple[int], num_classes: int,
+                 block=BasicBlock, layers=[2, 2, 2, 2], size_for_cifar=True):
+        # Argument `input_dim` is only used to check whether 'size_for_cifar' is True.
+
+        self.inplanes = 64
+        super(ResNet18, self).__init__()
+        input_dim = tuple(input_dim)
+        num_classes = int(num_classes)
+        size_for_cifar = True if input_dim[0] == 3 else False  #
+        self.size_for_cifar = size_for_cifar
+        num_channels = 3 if size_for_cifar else 1
+
+        self.conv1 = nn.Conv2d(num_channels, 64, kernel_size=3, stride=1, padding=1,
+                               bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.relu = nn.ReLU(inplace=True)
+        self.layer1 = self._make_layer(block, 64, layers[0])
+        self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
+        self.layer3 = self._make_layer(block, 256, layers[2], stride=2)
+
+        if size_for_cifar:
+            self.layer4 = self._make_layer(block, 512, layers[3], stride=2)
+            self.avgpool = nn.AvgPool2d(4)
+            self.fc = nn.Linear(512 * block.expansion, num_classes)
+        else:
+            self.avgpool = nn.AvgPool2d(7)
+            self.fc = nn.Linear(256 * block.expansion, num_classes)
+
+        # Initialize weights
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2. / n))
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+
+    def _make_layer(self, block, planes, blocks, stride=1):
+        downsample = None
+        if stride != 1 or self.inplanes != planes * block.expansion:
+            downsample = nn.Sequential(
+                nn.Conv2d(self.inplanes, planes * block.expansion,
+                          kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(planes * block.expansion),
+            )
+
+        layers = []
+        layers.append(block(self.inplanes, planes, stride, downsample))
+        self.inplanes = planes * block.expansion
+        for i in range(1, blocks):
+            layers.append(block(self.inplanes, planes))
+
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+
+        # We only use the final layer if `x` started as (-1, 3, 32, 32)
+        if self.size_for_cifar:
+            x = self.layer4(x)
+
+        x = self.avgpool(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+
+        return x
+
+    def get_conv_segment(self) -> nn.Sequential:
+        if self.size_for_cifar:
+            return nn.Sequential(self.conv1, self.bn1, self.relu,
+                                 self.layer1, self.layer2, self.layer3, self.layer4,
+                                 self.avgpool)
+        else:
+            return nn.Sequential(self.conv1, self.bn1, self.relu,
+                                 self.layer1, self.layer2, self.layer3,
+                                 self.avgpool)
+
+    def get_fc_segment(self) -> nn.Sequential:
+        return nn.Sequential(self.fc)
+
+    def calculate_conv_output(self, input_dim: tuple[int]) -> tuple[int, int, int]:
+        # This method is useless in this file! Because the input size is already computed in the original code.
+
+        # Assuming input_dim is a tuple (channels, height, width)
+        channels, height, width = input_dim
+
+        def conv2d_out_size(size, kernel_size=3, stride=1, padding=1):
+            return (
+                (size[0] - kernel_size + 2 * padding) // stride + 1,
+                (size[1] - kernel_size + 2 * padding) // stride + 1,
+            )
+
+        def maxpool2d_out_size(size, kernel_size=2, stride=2, padding=0):
+            return (
+                (size[0] - kernel_size + 2 * padding) // stride + 1,
+                (size[1] - kernel_size + 2 * padding) // stride + 1,
+            )
+
+        return (0, 0, 0,)
+
+
+if __name__ == "__main__":
+    # Example usage
+    input_dim = (3, 28, 28)  # Example input dimensions (channels, height, width)
+    num_classes = 10  # Example number of output classes
+    model = ResNet18(input_dim, num_classes)
+    print(model)
+
+    x = torch.randn(1, 3, 28, 28)
+    y = model(x)
+    print(y.shape)
+
+    print(model.get_conv_segment())
+    conv_segment = model.get_conv_segment()
+    y = conv_segment(x)
+    print(y.shape)
